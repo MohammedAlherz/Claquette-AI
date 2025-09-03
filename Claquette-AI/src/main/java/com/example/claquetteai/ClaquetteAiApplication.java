b0892ef5e6bdf879ca98681d8aaa5214cd6afae6
MohammedAlherz <moh300moh43@gmail.com>
2025-09-03 17:58:10 +0300

    first commit

---
+++ Claquette-AI/src/main/java/com/example/claquetteai/ClaquetteAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.claquetteai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaquetteAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaquetteAiApplication.class, args);
+    }
+
+}
