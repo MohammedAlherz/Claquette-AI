fb59a60a4cda69d1ff045f73e77d85cd596b7a21
olmnlo <hosamag54@gmail.com>
2025-09-05 16:17:47 +0300

    payment by userID, Ai by userId, limit for free trial

--- Claquette-AI/src/main/java/com/example/claquetteai/Repository/UserRepository.java
+++ Claquette-AI/src/main/java/com/example/claquetteai/Repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.example.claquetteai.Repository;
 
+import com.example.claquetteai.Model.CompanySubscription;
 import com.example.claquetteai.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Integer> {
     User findUserById(Integer id);
 
+    User findUserByCompany_ActiveSubscription(CompanySubscription companyActiveSubscription);
 }
